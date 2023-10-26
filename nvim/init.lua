991c1c4cd07024f53a18f42c8c91aa2069d9e1fc
nukkua <marco.salazar.2014.com@gmail.com>
2023-10-26 15:38:16 -0400

    yae-edition

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,47 +9,64 @@ end
 
 -- Tabbing
 
+-- Tabbing
+local config_path = vim.fn.stdpath('config')
+package.path = config_path .. "/?.lua;" .. package.path
+package.path = config_path .. "/?/init.lua;" .. package.path
 
-require('packer').startup(function()
-  -- Packer puede administrarse a sí mismo
-  use {'wbthomason/packer.nvim',
-  'nvim-treesitter/nvim-treesitter',
-  run = ':TSUpdate',
-  'folke/tokyonight.nvim',
-  'neovim/nvim-lspconfig',
-  'tpope/vim-fugitive',
-  'airblade/vim-gitgutter',
-  'kyazdani42/nvim-tree.lua',
-  'kyazdani42/nvim-web-devicons',
-  'hoob3rt/lualine.nvim',
-  'tpope/vim-commentary',
-  -- 'hrsh7th/cmp-buffer',
-  -- 'hrsh7th/cmp-nvim-lsp',
-  -- 'nvim-lua/telescope.nvim',
-  -- 'nvim-telescope/telescope-fzf-native.nvim',
-  -- 'ThePrimeagen/harpoon',
 
-  'windwp/nvim-autopairs', 
-  'hrsh7th/nvim-compe',
-  'junegunn/fzf.vim',
-  'windwp/nvim-ts-autotag',
-  'mattn/emmet-vim',
+vim.opt.termguicolors = true
+
+require('packer').startup(function(use)
+    -- Packer can manage itself
+    use ('wbthomason/packer.nvim')
+	use ('tpope/vim-commentary')
+	use ('kyazdani42/nvim-web-devicons')
+	use ('junegunn/fzf.vim')
+	use ('mattn/emmet-vim')
+    -- use {
+    --     'nvim-telescope/telescope.nvim', tag = '0.1.3',
+    --     -- or                            , branch = '0.1.x',
+    --     requires = { {'nvim-lua/plenary.nvim'} }
+    -- }
+
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+    use('nvim-treesitter/playground')
+    -- use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use('nvim-treesitter/nvim-treesitter-context')
+    use{
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
+
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
     }
 
 end)
-
-
-require'keymappings'
-require'plugins.lualine'
-require'plugins.tree'
-require'plugins.webdevicons'
-require'plugins.treesiter'
-require'plugins.lsp'
-require'plugins.autopairs'
-require'plugins.compe'
-
-
-vim.cmd[[colorscheme tokyonight]]
+-- vim.cmd[[colorscheme tokyonight]]
 vim.cmd[[set background=dark]]
 vim.cmd[[ hi Normal guibg=NONE ctermbg=NONE ]]
 vim.cmd[[ hi NormalNC guibg=NONE ctermbg=NONE ]]
@@ -68,5 +85,17 @@ vim.cmd[[highlight NvimTreeSpecialFile guifg=#FFC0CB]]
 vim.cmd('highlight NvimTreeNormal guibg=NONE')
 vim.cmd('highlight NvimTreeNormalNC guibg=NONE')
 vim.cmd [[let g:user_emmet_leader_key='<C-y>']]
+
+require'colorsget'
+require'keymappings'
+require'plugins.lualine'
+require'plugins.undotree'
+-- require'plugins.tree'
+require'plugins.webdevicons'
+require'plugins.treesiter'
+require'plugins.lsp'
+-- require'plugins.autopairs'
+-- require'plugins.compe'
+
 
 
--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -1,28 +1,54 @@
+local lsp = require('lsp-zero')
+
+lsp.on_attach(function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+	--lsp.default_keymaps({buffer = bufnr})
+
+    vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, opts)
+
+	vim.keymap.set('n', '<leader>vd', function() end, opts)
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+	ensure_installed = {
+		'svelte',
+		'gopls',
+		'lua_ls',
+		'rust_analyzer',
+		'jedi_language_server',
+        'omnisharp',
+        'ansiblels',
+        'cssls',
+        'dockerls',
+        'docker_compose_language_service',
+        'eslint',
+        'html',
+        'jsonls',
+        'tsserver',
+        'marksman',
+        'taplo',
+        'yamlls'
+	},
+	handlers = {
+		lsp.default_setup
+	}
+})
+
 local lspconfig = require('lspconfig')
-lspconfig.unocss.setup{}
-lspconfig.pyright.setup{}
-lspconfig.intelephense.setup{}
-lspconfig.gopls.setup{}
-lspconfig.rust_analyzer.setup{}
-lspconfig.clangd.setup{}
-lspconfig.html.setup{}
-lspconfig.tsserver.setup{}
--- lspconfig.javascript.setup{}
-lspconfig.svelte.setup{
-   cmd = { "svelteserver", "--stdio" },
-   filetypes = { "svelte" },
-   root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
-   settings = {
-      svelte = {
-         plugin = {
-            svelte = {
-               format = {
-                  enable = true
-               }
-            }
-         }
-      }
-   }
-}
+lspconfig.omnisharp.setup({})
 
-
+local cmp = require('cmp')
+local cmp_action = lsp.cmp_action()
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+cmp.setup({
+--	sources = {
+--		{ name = 'nvim_lsp' }
+--	}, 
+	mapping = cmp.mapping.preset.insert({
+		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+		['<Tab>'] = cmp.mapping.confirm({ select = true }),
+		['<C-Space>'] = cmp.mapping.complete()
+	})
+})
